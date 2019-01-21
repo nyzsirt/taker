745cc9bc4d0a720e0db0e70cfe49f2b0a5ddc5c2
Niyazi Sırt <niyazi.sirt@labrisnetworks.com>
2019-01-21 21:57:19 +0300

    devel: change interval

--- taker/config/config.py
+++ taker/config/config.py
@@ -100,11 +100,10 @@ DASHBOARD_IP = "0.0.0.0" #Bind the dashboard to this IP. Default is 0.0.0.0, use
 
 
 SCHEDULER_TASKS = [
-    # {"path": "tasks.tcmb.Tcmb", "params": {}, "interval": 1, "queue": "tcmb"},
-    # {"path": "tasks.isbank.IsBank", "params": {}, "interval": 1, "queue": "isbank"},
-    # # {"path": "tasks.yapikredi.YapiKredi", "params": {}, "interval": 1, "queue": "yapikredi"},
-    {"path": "tasks.investing.Investing", "params": {}, "interval": 1, "queue": "default"},
-    {"path": "tasks.bloomberg.Bloomberg", "params": {}, "interval": 1, "queue": "default"},
+    {"path": "tasks.tcmb.Tcmb", "params": {}, "interval": 2, "queue": "default"},
+    {"path": "tasks.isbank.IsBank", "params": {}, "interval": 2, "queue": "default"},
+    {"path": "tasks.investing.Investing", "params": {}, "interval": 2, "queue": "default"},
+    {"path": "tasks.bloomberg.Bloomberg", "params": {}, "interval": 2, "queue": "default"},
 ]
 
 TASKS = {}
