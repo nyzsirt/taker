7516e3b7b287468c3c958d868b11571cbf3d966f
Niyazi Sırt <niyazi.sirt@labrisnetworks.com>
2019-02-27 22:08:20 +0300

    devel

--- taker/test/outer.py
+++ taker/test/outer.py
@@ -12,8 +12,8 @@ from pprint import pprint
 parser = argparse.ArgumentParser(description='Add assets to tags from file.')
 
 parser.add_argument('--currency', dest='currency', action='store', required=True, help='"eur" veya "usd"')
-parser.add_argument('--start', dest='start', action='store', required=True, help='baslangic tarihi: format Yyyymmddhhmmss')
-parser.add_argument('--end', dest='end', action='store', required=True, help='bitis tarihi: format Yyyymmddhhmmss')
+parser.add_argument('--start', dest='start', action='store', required=False, default= "",  help='baslangic tarihi: format Yyyymmddhhmmss')
+parser.add_argument('--end', dest='end', action='store', required=False, default= "", help='bitis tarihi: format Yyyymmddhhmmss')
 time_format = "%Y%m%d%H%M%S"
 
 
@@ -33,43 +33,58 @@ def _flatten(data):
 if __name__ == '__main__':
     setup_context(file_path=BASEPATH + 'taker/config/config.py', config_type='run')
     args = parser.parse_args()
-    start_timestamp = mktime(utc.localize(datetime.strptime(args.start, time_format)).utctimetuple())
-    end_timestamp = mktime(utc.localize(datetime.strptime(args.end, time_format)).utctimetuple())
+    _query = {}
+    if args.start:
+        start_timestamp = mktime(utc.localize(datetime.strptime(args.start, time_format)).utctimetuple())
+        end_timestamp = mktime(utc.localize(datetime.strptime(args.end, time_format)).utctimetuple())
+        _query = {
+            "timestamp": {
+                "$gte": start_timestamp, "$lt": end_timestamp,
+            }
+        }
 
     if args.currency == "usd":
         usd_collection = connections.mongodb_jobs.ex_usd_try
 
-        bloomberg = usd_collection.find({"type": PARSER_TYPE_BLOOMBERG, "timestamp": {"$gte": start_timestamp, "$lt": end_timestamp}})
+        _query["type"] = PARSER_TYPE_BLOOMBERG
+        bloomberg = usd_collection.find(_query)
         with open("usd-bloomberg-%s-%s.csv" % (args.start, args.end), "w") as ff:
             ff.writelines(_flatten(bloomberg))
 
-        investing = usd_collection.find({"type": PARSER_TYPE_INVESTING, "timestamp": {"$gte": start_timestamp, "$lt": end_timestamp}})
+        _query["type"] = PARSER_TYPE_INVESTING
+        investing = usd_collection.find(_query)
         with open("usd-investing-%s-%s.csv" % (args.start, args.end), "w") as ff:
             ff.writelines(_flatten(investing))
 
-        isbank = usd_collection.find({"type": PARSER_TYPE_ISBANK, "timestamp": {"$gte": start_timestamp, "$lt": end_timestamp}})
+        _query["type"] = PARSER_TYPE_ISBANK
+        isbank = usd_collection.find(_query)
         with open("usd-isbank-%s-%s.csv" % (args.start, args.end), "w") as ff:
             ff.writelines(_flatten(isbank))
 
-        tcmb = usd_collection.find({"type": PARSER_TYPE_TCMB, "timestamp": {"$gte": start_timestamp, "$lt": end_timestamp}})
+        _query["type"] = PARSER_TYPE_TCMB
+        tcmb = usd_collection.find(_query)
         with open("usd-tcmb-%s-%s.csv" % (args.start, args.end), "w") as ff:
             ff.writelines(_flatten(tcmb))
 
     if args.currency == "eur":
+        _query["type"] = PARSER_TYPE_BLOOMBERG
         eur_collection = connections.mongodb_jobs.ex_eur_try
-        bloomberg = eur_collection.find({"type": PARSER_TYPE_BLOOMBERG, "timestamp": {"$gte": start_timestamp, "$lt": end_timestamp}})
+        bloomberg = eur_collection.find(_query)
         with open("eur-bloomberg-%s-%s.csv" % (args.start, args.end), "w") as ff:
             ff.writelines(_flatten(bloomberg))
 
-        investing = eur_collection.find({"type": PARSER_TYPE_INVESTING, "timestamp": {"$gte": start_timestamp, "$lt": end_timestamp}})
+        _query["type"] = PARSER_TYPE_INVESTING
+        investing = eur_collection.find(_query)
         with open("eur-investing-%s-%s.csv" % (args.start, args.end), "w") as ff:
             ff.writelines(_flatten(investing))
 
-        isbank = eur_collection.find({"type": PARSER_TYPE_ISBANK, "timestamp": {"$gte": start_timestamp, "$lt": end_timestamp}})
+        _query["type"] = PARSER_TYPE_ISBANK
+        isbank = eur_collection.find(_query)
         with open("eur-isbank-%s-%s.csv" % (args.start, args.end), "w") as ff:
             ff.writelines(_flatten(isbank))
 
-        tcmb = eur_collection.find({"type": PARSER_TYPE_TCMB, "timestamp": {"$gte": start_timestamp, "$lt": end_timestamp}})
+        _query["type"] = PARSER_TYPE_TCMB
+        tcmb = eur_collection.find(_query)
         with open("eur-tcmb-%s-%s.csv" % (args.start, args.end), "w") as ff:
             ff.writelines(_flatten(tcmb))
 
